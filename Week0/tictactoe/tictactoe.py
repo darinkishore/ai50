6420590ed452a3a1f79056086e042a1ddd2c6791
darinkishore <darinkishore@protonmail.com>
2022-08-03 17:59:21 -0700

    implemented minmax, attempted to add alpha-beta pruning

--- Week0/tictactoe/tictactoe.py
+++ Week0/tictactoe/tictactoe.py
@@ -73,14 +73,14 @@ def winner(board):
         col_list = []
         for row in range(3):
             col_list += [board[row][col]]
-        if col_list.count(col_list[0]) == 3:
+        if col_list.count(col_list[0]) == 3:  # there are 3 X's or O's in one of the cols
             return col_list[0]
 
     # check two diagonals for possible winners
     # TODO: Double check if there are any diagonal edge cases not covered by these.
     if (board[0][0] == board[1][1]) and (board[1][1] == board[2][2]):
-        return board[1][1] # arbitrary, can be any of these 3 vals.
-    if (board[0][2] == board [1][1]) and (board[1][1] == board[2][0]):
+        return board[1][1]  # arbitrary, can be any of these 3 vals.
+    if (board[0][2] == board[1][1]) and (board[1][1] == board[2][0]):
         return board[1][1]
 
     return None
@@ -103,9 +103,9 @@ def utility(board):
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
     win = winner(board)
-    if win == "X":
+    if win == X:
         return 1
-    elif win == "O":
+    elif win == O:
         return -1
     else:
         return 0
@@ -113,28 +113,60 @@ def utility(board):
 
 def minimax(board):
     """
-    Returns the optimal action for the current player on the board.
+    Given a state s:
+
+    The maximizing player picks action a in Actions(s) that produces the highest value of Min-Value(Result(s, a)).
+    The minimizing player picks action a in Actions(s) that produces the lowest value of Max-Value(Result(s, a)).
     """
+    if terminal(board):
+        return None
+
     if player(board) == X:
-        possibilities = actions(board)
-        for action in possibilities:
+        possibilities = [(result(board, action), action) for action in actions(board)]
+        best_action = possibilities[0][1]  # first value of first tuple
+        max_act = min_value(possibilities[0][0], -math.inf)  # unsure if this is right way to indicate
+        possibilities.remove(possibilities[0])
+
+        for possible_board in possibilities:
+            current_min = min_value(possible_board[0], max_act)
+            if max_act > current_min:
+                continue
+            else:
+                max_act = current_min
+                best_action = possible_board[1]
+        return best_action
+
+    if player(board) == O:
+        possibilities = [(result(board, action), action) for action in actions(board)]
+        best_action = possibilities[0][1]
+        min_act = max_value(possibilities[0][0], math.inf)
+        possibilities.remove(possibilities[0])
+
+        for possible_board in possibilities:
+            current_max = max_value(possible_board[0], min_act)
+            if min_act < current_max:
+                continue
+            else:
+                min_act = current_max  # number to compare to
+                best_action = possible_board[1]
+        return best_action  # == min_act
 
 
-    raise NotImplementedError
-
-def min_value(board):
+def min_value(board, curr_min):
     v = math.inf
     if terminal(board):
         return utility(board)
     for action in actions(board):
-        v = max(v, max_value(result(board, action)))
+        if v > curr_min:
+            v = min(v, max_value(result(board, action)))
     return v
 
-def max_value(board):
+
+def max_value(board, curr_max):
     v = -math.inf
     if terminal(board):
         return utility(board)
-    for action in actions:
-        v = max(v, min_value(result(board, action)))
+    for action in actions(board):
+        if v < curr_max:  # may not work!
+            v = max(v, min_value(result(board, action)))
     return v
-
